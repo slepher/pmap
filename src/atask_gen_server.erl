b8ec9063567b71a9c1c3d1b10b2013d6e3cf2fe7
slepher <slepher.chen@taodinet.com>
2013-08-06 14:39:07 +0800

    add fun bindl

--- src/atask_gen_server.erl
+++ src/atask_gen_server.erl
@@ -10,7 +10,8 @@
 
 %% API
 -export([call/2, message/2, reply_async/4]).
--export([wait_reply/4, wait_reply/5, handle_reply/3, pure_handle_reply/3]).
+-export([wait_reply/2, wait_reply/3, wait_reply/4, wait_reply/5,
+         handle_reply/3, pure_handle_reply/3]).
 -export([state/1]).
 
 %%%===================================================================
@@ -23,6 +24,38 @@ call(Process, Request) ->
 message({PId, MRef}, Message) ->
     PId ! {message, MRef, Message}.
 
+bind(Callback, Async, NextAsyncFun, Offset, State) ->
+    atask_gen_server:wait_reply(
+      Async,
+      fun({ok, Val}) ->
+              atask_gen_server:wait_reply(
+                NextAsyncFun(),
+                Callback({ok, Val})
+               );
+         ({error, Reason})->
+              Callback({error, Reason}),
+              State
+      end, Offset, State).
+
+
+bindl(Callback, _Async, [], Acc) ->
+    Callback(ok, ok, Acc),
+    fun(_Offset, State) ->
+            State
+    end;
+bindl(Callback, Async, [Arg|T], Acc) ->
+    atask_gen_server:wait_reply(
+      Async(Arg), 
+      fun({ok, Val}) ->
+              NAcc = Callback(Arg, {ok, Val}, Acc),
+              bindl(Async, Callback, T, NAcc);
+         ({error, Reason}) ->
+              Callback(Arg, {error, Reason}, Acc)
+      end).
+
+return(Fun, Offset, State) ->
+    Fun(Offset, State).
+
 reply_async(MRef, From, Offset, State) ->
     wait_reply(
       fun(Reply, S) ->
@@ -30,18 +63,33 @@ reply_async(MRef, From, Offset, State) ->
               S
       end, MRef, Offset, State).
 
-wait_reply(Callback, MRef, Offset, State) ->
+wait_reply(MRef, Callback) ->
+    wait_reply(MRef, Callback, infinity).
+
+wait_reply(MRef, Callback, Timeout) ->
+    fun(Offset, State) ->
+            wait_reply(MRef, Callback, Offset, State, Timeout)
+    end.
+
+wait_reply(MRef, Callback, Offset, State) when is_reference(MRef), is_function(Callback) ->
+    wait_reply(Callback, MRef, Offset, State, infinity);
+
+wait_reply(Callback, MRef, Offset, State) when is_reference(MRef), is_function(Callback) ->
     wait_reply(Callback, MRef, Offset, State, infinity).
 
 wait_reply(Callback, {ok, MRef}, Offset, State, Timeout) when is_reference(MRef) ->
     wait_reply(Callback, MRef, Offset, State, Timeout);
-wait_reply(Callback, MRef, Offset, State, Timeout) when is_reference(MRef) ->
+wait_reply(MRef, Callback, Offset, State, Timeout)
+  when is_reference(MRef), is_function(Callback) ->
+    wait_reply(Callback, MRef, Offset, State, Timeout);
+wait_reply(Callback, MRef, Offset, State, Timeout) 
+  when is_reference(MRef), is_function(Callback) ->
     NCallback = atask:wait_reply(Callback, MRef, Timeout),
     Callbacks = element(Offset, State),
     NCallbacks = store(MRef, NCallback, Callbacks),
     setelement(Offset, State, NCallbacks);
-wait_reply(Callback, Reply, _Offset, State, _Timeout) ->
-    Callback(Reply, State).
+wait_reply(Callback, Reply, Offset, State, _Timeout) ->
+    execute_callback(Callback, Reply, Offset, State).
 
 handle_reply(Reply, Offset, State) ->
     StateRec = element(1, State),
@@ -57,7 +105,7 @@ pure_handle_reply({message, MRef, Message}, Offset, State) when is_reference(MRe
     Callbacks = element(Offset, State),
     case find(MRef, Callbacks) of
         {ok, Callback} ->
-            Callback({message, Message}, State);
+            execute_callback(Callback, {message, Message}, Offset, State);
         error ->
             State
     end;
@@ -116,5 +164,18 @@ erase(Key, Dict) when is_list(Dict) ->
 erase(Key, Dict) ->
     dict:erase(Key, Dict).
 
-
-
+execute_callback(Callback, Reply, Offset, State) ->
+    case erlang:fun_info(Callback) of
+        {arity, 1} ->
+            case Callback(Reply) of
+                NCallback when is_function(NCallback) ->
+                    NCallback(Offset, State);
+                _Other ->
+                    State   
+            end;
+        {arity, 2} ->
+            Callback(Reply, State);
+        {arity, N} ->
+            exit({invalid_callback, Callback, N})
+    end.
+            
